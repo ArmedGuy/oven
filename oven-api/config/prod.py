e4e83986c45898e8554f6fdf13db2dd2bf892a77
Marcus Lund <amuulo-4@student.ltu.se>
2017-12-07 02:11:44 +0100

    Using static random keys for session.

--- oven-api/config/prod.py
+++ oven-api/config/prod.py
@@ -2,6 +2,6 @@ import os
 
 class ProductionConfig:
 	DEBUG = False
-	SECRET_KEY = os.urandom(24)
+	SECRET_KEY = "@\x0e\xf1t\x15{\xa1x\xc2\xfb\xba\x01\xc5\xc0\x1b\x05\x0b+@\x0eT?\xbb\x1b"
 	TEMPLATES_AUTO_RELOAD = False
 	SESSION_COOKIE_NAME = 'oven_api_session'
